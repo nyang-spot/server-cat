26f79f5192a0ebd64525d5ddb0e6fd75aed10ecc
ehgks0000 <ehgks0083@gmail.com>
2022-10-15 23:57:45 +0900

    Fix: Users 테이블 수정
    * location, createdAt, updatedAt, deletedAt 추가

--- src/usres/controllers/loginController.ts
+++ src/usres/controllers/loginController.ts
@@ -11,10 +11,12 @@ export const loginController = async (
   { query: { location: _location } }: Request<unknown, unknown, { name: string }, { location: string }>,
   res: Response
 ) => {
+  // @TODO 추후에 카카오 소셜 로그인 구현할 때 마이페이지에서 수정을 하거나
+  // 소셜 로그인 후 바로 name 수정 페이지로 이동하게 해서 넣어 주는 것으로 구현
   const name = `user${randomString()}`;
-  // const location = _location || "gannamgu";
+  const location = _location || "gannamgu";
 
-  const user = await loginService({ name }, Prisma);
+  const user = await loginService({ name, location }, Prisma);
 
   res.json({ user });
 };
--- src/usres/services/loginService.ts
+++ src/usres/services/loginService.ts
@@ -1,7 +1,7 @@
 import { type PrismaClient } from "@prisma/client";
 
-export const loginService = async ({ name }: { name: string }, prisma: PrismaClient) => {
-  const user = await prisma.users.create({ data: { name } });
+export const loginService = async ({ name, location }: { name: string; location: string }, prisma: PrismaClient) => {
+  const user = await prisma.users.create({ data: { name, location } });
   // const user = await prisma.users.findFirst({ where: { name } });
 
   return user;
