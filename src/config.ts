9a1616f4308698f580d7a82763a77bfe4b136758
ehgks0000 <ehgks0083@gmail.com>
2022-10-15 22:15:45 +0900

    Add: express 및 cors 추가
    * express 추가 및 bodyparser 설정
    * cors 추가

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const CORS_CONFIG = {
+    origin: "*"
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("street-cat-fighter Server !!");
+import express from "express";
+import cors from "cors";
+import { CORS_CONFIG } from "./config";
+
+const PORT = 3000;
+
+export const main = async () => {
+    const app = express();
+
+    app.use(cors(CORS_CONFIG));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    return app;
+};
+
+(async () => {
+    const app = await main();
+
+    app.listen(PORT, () => {
+        console.log("street-cat-fighter Server !!");
+    });
+})();
