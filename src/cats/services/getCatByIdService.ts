85fc2cfd16347cf496f3d9e017403ee7553da467
ehgks0000 <ehgks0083@gmail.com>
2022-10-16 21:36:47 +0900

    Fix: getCatById 수정
    * likes count를 객체로 감싸줘서 반환

--- src/cats/services/getCatByIdService.ts
+++ src/cats/services/getCatByIdService.ts
@@ -1,9 +1,12 @@
 import type { PrismaClient } from "@prisma/client";
 
 export const getCatByIdService = async ({ id, catId, location }: { id: number; catId: number; location: string }, prisma: PrismaClient) => {
-  const cat = await prisma.cats.findFirst({ where: { id: catId, location }, select: { id: true, description: true, image: true, location: true } });
+  const cat = await prisma.cats.findFirst({
+    where: { id: catId, location },
+    select: { id: true, description: true, image: true, location: true, _count: { select: { likes: true } } }
+  });
   // 좋아요 수, 내가 좋아요 했는지.
-  const likes = await prisma.likes.count({ where: { userId: id, catId } });
+  // const likes = await prisma.likes.count({ where: { catId } });
   const myLike = await prisma.likes.findUnique({ where: { userId_catId: { userId: id, catId } } });
-  return cat && { ...cat, likes, ...(myLike && { isLiked: true }) };
+  return cat && { ...cat, isLiked: !!myLike };
 };
