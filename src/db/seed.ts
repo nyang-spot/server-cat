dc715e6dbd7022cfc85bc064b915814b78721053
ehgks0000 <ehgks0083@gmail.com>
2022-10-17 13:59:38 +0900

    Add: seed file
    * seed.ts 수정 및 이미지 파일 추
    가

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,24 +1,30 @@
+import { HOST, PORT } from "../config";
 import prisma from "./index";
 
 if (require.main === module) {
   (async () => {
     const user = await prisma.users.create({
       data: {
-        name: "test user",
+        name: "test-user-2",
         location: "gannamgu"
       }
     });
+    const dataArr = [];
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < 10; i++) {
+      const imageUrl = `${HOST}:${PORT}/${i}.jpg`;
+
+      dataArr.push({
+        description: `seed-${i}`,
+        location: user.location,
+        image: imageUrl,
+        latitude: 37.496486063,
+        longitude: 127.028361548,
+        authorId: user.id
+      });
+    }
     const cats = await prisma.cats.createMany({
-      data: [
-        {
-          description: "seed 1",
-          location: user.location,
-          image: "이미지파일?",
-          latitude: 10,
-          longitude: 10,
-          authorId: user.id
-        }
-      ]
+      data: [...dataArr]
     });
 
     await prisma.likes.create({
