e8660acb62b88cce6f90a383d181d7b8415ff556
ehgks0000 <ehgks0083@gmail.com>
2022-11-08 00:43:51 +0900

    Feat: add looger using winston
    * requestLoggerMiddleware
    * errorMiddleware

--- src/usres/services/loginService.ts
+++ src/usres/services/loginService.ts
@@ -3,8 +3,8 @@ import { type PrismaClient } from "@prisma/client";
 /**
  * @depreated Use auth/routes
  */
-export const loginService = async ({ name, location }: { name: string; location: string }, prisma: PrismaClient) => {
-  const email = "test@gmail.com";
+export const loginService = async ({ email, name, location }: { email: string; name: string; location: string }, prisma: PrismaClient) => {
+  // const email = "test@gmail.com";
   const user = await prisma.users.create({ data: { name, location, email }, select: { id: true, name: true, location: true } });
 
   return user;
